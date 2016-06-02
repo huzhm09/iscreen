c3fedd57f4c71838a2a7e8131b803224d4796cd4
huzhm09 <huzhm09@163.com>
2016-06-02 14:18:05 +0800

    修改内容
    修改申请页面

--- iscreen/Lib/Action/IndexAction.class.php
+++ iscreen/Lib/Action/IndexAction.class.php
@@ -1,7 +1,7 @@
 <?php
 // 系统首页
 class IndexAction extends Action {
-   //展示首页
+    //展示首页
     public function Index(){
    		//获取focus链接
    		$Focus	=	M("focus_img");
@@ -10,5 +10,11 @@ class IndexAction extends Action {
    		$this->info  =	$Focus->where($where)->select();
    		//显示模板
    		$this->display();
+    }
+
+    //申请页
+    public function Appl(){
+      //显示模板
+      $this->display();
     }
 }
