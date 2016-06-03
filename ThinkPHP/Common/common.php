deaa7a9901a01d5c133c02b2aba5aa0f2fa80cbe
huzhm09 <huzhm09@163.com>
2016-06-03 10:53:20 +0800

    完善展示页面
    完善展示面

--- ThinkPHP/Common/common.php
+++ ThinkPHP/Common/common.php
@@ -755,3 +755,34 @@ function trace($value='[think]',$label='',$level='DEBUG',$record=false) {
         }
     }
 }
+
+/**
+ * 截取中文字符串函数
+ * @param string $str 字符
+ * @param int $start 开始位置
+ * @param int $length 长度
+ * @param string $charset 字符编码
+ * @param string $suffix 是否以省略号代替
+ * @return void
+ */
+function msubstr($str, $start=0, $length, $charset="utf-8", $suffix=false){
+     if(function_exists("mb_substr")){
+      if($suffix)
+       return mb_substr($str, $start, $length, $charset)."...";
+      else
+       return mb_substr($str, $start, $length, $charset);
+     }elseif(function_exists('iconv_substr')) {
+      if($suffix)
+       return iconv_substr($str,$start,$length,$charset)."...";
+      else
+       return iconv_substr($str,$start,$length,$charset);
+     }
+     $re['utf-8'] = "/[x01-x7f]|[xc2-xdf][x80-xbf]|[xe0-xef][x80-xbf]{2}|[xf0-xff][x80-xbf]{3}/";
+     $re['gb2312'] = "/[x01-x7f]|[xb0-xf7][xa0-xfe]/";
+     $re['gbk'] = "/[x01-x7f]|[x81-xfe][x40-xfe]/";
+     $re['big5'] = "/[x01-x7f]|[x81-xfe]([x40-x7e]|xa1-xfe])/";
+     preg_match_all($re[$charset], $str, $match);
+     $slice = join("",array_slice($match[0], $start, $length));
+     if($suffix) return $slice."…";
+     return $slice;
+}
--- iscreen/Lib/Action/IndexAction.class.php
+++ iscreen/Lib/Action/IndexAction.class.php
@@ -20,7 +20,57 @@ class IndexAction extends Action {
 
     //大屏幕展示
     public function Show(){
-      //显示模板
+      //实例化数据库
+      $screen = M("screen");
+      //导入数据类
+      import("ORG.Util.Page");
+      //获取总数据
+      $count  = $screen->where("status=1")->count();
+      //实例化分页类
+      $page   = new page($count,24);
+      //设置分页样式
+      $page->setConfig("theme","%totalRow% %header% %nowPage%/%totalPage% 页 %first% %upPage% %downPage% %prePage% %linkPage% %nextPage% %end%");
+      //查询分页数据
+      $list   = $screen->where("status=1")->order("id desc")->limit($page->firstRow.",".$page->listRows)->select(); 
+      //获取分页展示
+      $show   = $page->show();
+      //赋值变量
+      $this->assign("show",$show);
+      $this->assign("list",$list);
+      //输出模板
+      $this->display(); 
+    }
+
+    //按学校查询
+    public function School(){
+      //接收变量
+      $school  = I("get.s");
+      //实例化数据库
+      $screen = M("screen");
+      //导入数据类
+      import("ORG.Util.Page");
+      //获取总数据
+      $count  = $screen->where(array("status"=>'1',"area"=>$school))->count();
+      //实例化分页类
+      $page   = new page($count,24);
+      //设置分页样式
+      $page->setConfig("theme","%totalRow% %header% %nowPage%/%totalPage% 页 %first% %upPage% %downPage% %prePage% %linkPage% %nextPage% %end%");
+      //查询分页数据
+      $list   = $screen->where(array("status"=>'1',"area"=>$school))->order("id desc")->limit($page->firstRow.",".$page->listRows)->select(); 
+      //获取分页展示
+      $show   = $page->show();
+      //赋值变量
+      $this->assign("show",$show);
+      $this->assign("list",$list);
+      //输出模板
+      $this->display(); 
+    }
+
+    //关键词展示页面
+    public function action(){
+      //接收变量
+      $this->key = I("get.t");
+      //调模板
       $this->display();
     }
 }
